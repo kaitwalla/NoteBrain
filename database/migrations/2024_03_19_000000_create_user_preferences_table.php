eea8ab79bc74d720459f302c7612e2f211a0fb69
Kait <git@kait.email>
2025-06-11 14:17:58 -0700

    fix migration

--- database/migrations/2024_03_19_000000_create_user_preferences_table.php
+++ database/migrations/2024_03_19_000000_create_user_preferences_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,14 +13,7 @@ return new class extends Migration
         Schema::create('user_preferences', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->json('article_preferences')->default(json_encode([
-                'font_size' => 1.25,
-                'paragraph_spacing' => 2,
-                'content_width' => '4xl',
-                'font_family' => 'system',
-                'line_height' => 1.5,
-                'articles_per_page' => 20,
-            ]));
+            $table->json('article_preferences')->default();
             $table->timestamps();
         });
     }
@@ -33,4 +25,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('user_preferences');
     }
-}; 
+};
