cab700489c70f228dae692e41c7777c2dfb88c11
Kait <git@kait.email>
2025-06-22 01:08:16 -0700

    another attempt

--- config/sanctum.php
+++ config/sanctum.php
@@ -78,7 +78,8 @@ return [
     'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+        // Disable CSRF validation for API token requests
+        // 'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
 
 ];
