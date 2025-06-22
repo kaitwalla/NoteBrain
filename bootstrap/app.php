c6c9eae14185c0c88353a3c02a6d52b12107e1d7
Kait <git@kait.email>
2025-06-22 01:05:35 -0700

    attempt fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: ['api/*']);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         \Spatie\LaravelFlare\Facades\Flare::handles($exceptions);
